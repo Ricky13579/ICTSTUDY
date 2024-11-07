8b9f499170c5656d824383f5b4f8c38be56f5388
wpzmfhtm <wpzmfhtm@naver.com>
2024-11-07 16:11:10 +0900

    자바 6일차(set 추가)

--- java_04/src/ch03/_02_Book.java
+++ java_04/src/ch03/_02_Book.java
@@ -6,16 +6,40 @@ public class _02_Book {
 	 * 		- 멤버변수(속성) + 멤버메서드(기능, 동작)
 	 * 객체 : 클래스를 바탕으로 만들어진 힙메모리 공간
 	 */
+//	int bookNo;
+//	String bookTitle;
+//	String author;
+	
 	// 멤버변수(속성)
-	int bookNo; 		// 책번호 1			2
-	String bookTitle;	// 책제목 java입문		JSP
-	String author;		// 책저자 박은종			최범균
+	private int bookNo; 		// 책번호 1			2
+	private String bookTitle;	// 책제목 java입문		JSP
+	private String author;		// 책저자 박은종			최범균
+	private int price;			// 가격 25000			35000
+	
 	
 	// 멤버메서드(기능, 동작)
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+	}
+	
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
 	public void showInfo() {
-		System.out.println("책 번호 : "+bookNo);
+		System.out.println("책 번호 : "+bookNo+"번");
 		System.out.println("책 제목 : "+bookTitle);
 		System.out.println("책 저자 : "+author);
+		System.out.println("책 가격 : "+price+"원");
 		System.out.println("--------------");
 		System.out.println();
 	}
--- java_04/src/ch03/_02_BookMain.java
+++ java_04/src/ch03/_02_BookMain.java
@@ -3,15 +3,31 @@ package ch03;
 public class _02_BookMain {
 	public static void main(String[] args) {
 		_02_Book book1 = new _02_Book();
-		book1.bookNo = 1;
-		book1.bookTitle = "java입문";
-		book1.author = "박은종";
+		
+//		book1.bookNo = 1;
+//		book1.bookTitle = "java입문";
+//		book1.author = "박은종";
+//		book1.price = 25000;
+//		book1.showInfo();
+		
+		book1.setBookNo(1);
+		book1.setBookTitle("java입문");
+		book1.setAuthor("박은종");
+		book1.setPrice(25000);
 		book1.showInfo();
 		
 		_02_Book book2 = new _02_Book();
-		book2.bookNo = 2;
-		book2.bookTitle = "JSP";
-		book2.author = "최범균";
+		
+//		book2.bookNo = 2;
+//		book2.bookTitle = "JSP";
+//		book2.author = "최범균";
+//		book2.price = 35000;
+//		book2.showInfo();
+		
+		book2.setBookNo(2);
+		book2.setBookTitle("JSP");
+		book2.setAuthor("최범균");
+		book2.setPrice(35000);
 		book2.showInfo();
 	}
 }
