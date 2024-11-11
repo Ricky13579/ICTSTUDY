2069e029e1e712da4b77a9699346e2ef59d5ed51
wpzmfhtm <wpzmfhtm@naver.com>
2024-11-11 14:58:55 +0900

    static 변수

---
+++ java_04/src/ch04/_06_StudentMain.java
@@ -0,0 +1,15 @@
+package ch04;
+
+public class _06_StudentMain {
+	public static void main(String[] args) {
+		_06_Student lee = new _06_Student();
+		lee.setName("이지원");
+		System.out.println("SerialNum : "+_06_Student.serialNum);
+		lee.studentInfo();
+		
+		_06_Student son = new _06_Student();
+		System.out.println("SerialNum : "+_06_Student.serialNum);
+		son.setName("손수경");
+		son.studentInfo();
+	}
+}
