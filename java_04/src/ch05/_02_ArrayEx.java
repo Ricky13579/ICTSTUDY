a21fb179167217be7eb165c6998e23b37cbc2176
wpzmfhtm <wpzmfhtm@naver.com>
2024-11-11 16:34:50 +0900

    자바 8일차(배열)

---
+++ java_04/src/ch05/_02_ArrayEx.java
@@ -0,0 +1,25 @@
+package ch05;
+
+public class _02_ArrayEx {
+	public static void main(String[] args) {
+		// 방법 2)
+		// 음료수)
+		String[] drink = new String[5];
+		drink[0] = "제로콜라";
+		drink[1] = "제로사이다";
+		drink[2] = "아이스 아메리카노";
+		drink[3] = "코코팜";
+		drink[4] = "바나나맛 우유";
+		for(int i = 0; i < drink.length; i++) {
+			System.out.println("제가 좋아하는 음료수 "+drink.length+"개 중 "+(i+1)+"번째는 "+drink[i]+"입니다.");
+		}
+		System.out.println();
+		
+		// 방법 4)
+		// 음식)
+		String[] food = {"부대찌개", "카레", "솥밥", "제육볶음", "볶음밥"};
+		for(int i = 0; i < food.length; i++) {
+			System.out.println("제가 좋아하는 음식 "+food.length+"개 중 "+(i+1)+"번째는 "+food[i]+"입니다.");
+		}
+	}
+}
