a5f40d1df649f5a97aaf3b6b2fdd35aaf1a26012
wpzmfhtm <wpzmfhtm@naver.com>
2024-11-11 16:51:01 +0900

    자바 8일차(향상된 for문)

--- java_04/src/ch05/_02_ArrayEx.java
+++ java_04/src/ch05/_02_ArrayEx.java
@@ -21,5 +21,16 @@ public class _02_ArrayEx {
 		for(int i = 0; i < food.length; i++) {
 			System.out.println("제가 좋아하는 음식 "+food.length+"개 중 "+(i+1)+"번째는 "+food[i]+"입니다.");
 		}
+		System.out.println();
+		
+		// 향상된 for문
+		// 배열 요소값을 변수에 하나씩 가져와서 변수에 대입 후 실행
+		// for(변수 : 배열){
+		// }
+		System.out.println("<<< 향상된 for문 >>>");
+		String[] foodArr = {"부대찌개", "카레", "솥밥", "제육볶음", "볶음밥"};
+		for(String arr : foodArr) {
+			System.out.println("제가 좋아하는 음식은 "+arr+"입니다.");
+		}
 	}
 }
