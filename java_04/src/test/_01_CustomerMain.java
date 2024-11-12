28822ec2bde6ff2ee019da664d1d960332ba801a
wpzmfhtm <wpzmfhtm@naver.com>
2024-11-12 11:56:37 +0900

    자바 9일차(생성자 복습)

---
+++ java_04/src/test/_01_CustomerMain.java
@@ -0,0 +1,20 @@
+package test;
+
+public class _01_CustomerMain {
+	public static void main(String[] args) {
+		System.out.println("<<< 객체 생성(디폴트 생성자) + setter >>>");
+		_01_Customer kim = new _01_Customer();
+		
+		kim.setCustomerID("C001");
+		kim.setCustomerName("김태희");
+		kim.setBonusPoint(10);
+		kim.setBonusRate(0.05);
+		
+		kim.customerInfo();
+		
+		// getter로 출력
+		System.out.println("<<< 객체 생성(매개변수 생성자) -> 멤버변수 >>>");
+		_01_Customer rain = new _01_Customer("C002", "비", 5, 0.03);
+		rain.customerInfo();
+	}
+}
