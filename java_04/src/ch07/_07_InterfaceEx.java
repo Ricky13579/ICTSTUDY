25f8adea8a294df697d76e86c7ad74c13c6a2b58
wpzmfhtm <wpzmfhtm@naver.com>
2024-11-15 10:55:18 +0900

    자바 13일차(복습 자료 -> 메서드 추가)

--- java_04/src/ch07/_07_InterfaceEx.java
+++ java_04/src/ch07/_07_InterfaceEx.java
@@ -10,17 +10,25 @@ public class _07_InterfaceEx {
 class First{
 	public void method1(Second two) {
 		System.out.println("First - Method1()");
-		two.method2();
+		two.method2(new Third());
 	}
 }
 
 class Second{
-	public void method2() {
+	public void method2(Third three) {
 		System.out.println("Second - method2()");
+		three.method3();
+	}
+}
+
+class Third{
+	public void method3() {
+		System.out.println("Third - method3()");
 	}
 }
 
 /*
  * First 클래스 - Method1()
  * Second 클래스 - Method2()
+ * Third 클래스 - Method3()
  */
