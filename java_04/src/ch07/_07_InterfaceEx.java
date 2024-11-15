af4a4e588604ea371a0a3984ebe6f788ff6382fa
wpzmfhtm <wpzmfhtm@naver.com>
2024-11-15 11:31:26 +0900

    자바 12일차(메서드 추가)

--- java_04/src/ch07/_07_InterfaceEx.java
+++ java_04/src/ch07/_07_InterfaceEx.java
@@ -9,21 +9,28 @@ public class _07_InterfaceEx {
 
 class First{
 	public void method1(Second two) {
-		System.out.println("First - Method1()");
+		System.out.println("First 클래스 - Method1()");
 		two.method2(new Third());
 	}
 }
 
 class Second{
 	public void method2(Third three) {
-		System.out.println("Second - method2()");
-		three.method3();
+		System.out.println("Second 클래스 - method2()");
+		three.method3(new Fourth());
 	}
 }
 
 class Third{
-	public void method3() {
-		System.out.println("Third - method3()");
+	public void method3(Fourth four) {
+		System.out.println("Third 클래스 - method3()");
+		four.method4();
+	}
+}
+
+class Fourth{
+	public void method4() {
+		System.out.println("Fourth 클래스 - method4()");
 	}
 }
 
