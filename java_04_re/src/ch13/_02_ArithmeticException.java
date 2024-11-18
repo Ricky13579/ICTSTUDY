111d8adb426a95681fe4998da66e9daa3b6def1b
wpzmfhtm <wpzmfhtm@naver.com>
2024-11-18 13:03:31 +0900

    코드 수정

--- java_04_re/src/ch13/_02_ArithmeticException.java
+++ java_04_re/src/ch13/_02_ArithmeticException.java
@@ -10,7 +10,7 @@ public class _02_ArithmeticException {
 			System.out.print("첫 번째 정수를 입력하세요 : ");
 			int a = sc.nextInt();
 			
-			System.out.print("첫 번째 정수를 입력하세요 : ");
+			System.out.print("두 번째 정수를 입력하세요 : ");
 			int b = sc.nextInt();
 			
 			System.out.println(a/b);
