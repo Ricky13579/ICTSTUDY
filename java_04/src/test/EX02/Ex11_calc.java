7720b26a8a1cff6975125a706d36bcbcf07314e1
wpzmfhtm <wpzmfhtm@naver.com>
2024-11-27 12:09:41 +0900

    자바 연습(인터페이스 - 사칙연산)

---
+++ java_04/src/test/EX02/Ex11_calc.java
@@ -0,0 +1,18 @@
+package test.EX02;
+
+public interface Ex11_calc {
+	double err = -3.14;
+	double error = -99;
+	
+	public int plus(int a, int b);
+	public int minus(int a, int b);
+	public int multi(int a, int b);
+	public double divide(int a, int b);
+	public void info();
+	
+	public void add(int c, int d);
+	public void sub(int c, int d);
+	public void mul(int c, int d);
+	public void div(int c, int d);
+	public void calcInfo();
+}
