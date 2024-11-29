abb27f52d6a9efb267ece910eea5f1ca8d9b972c
wpzmfhtm <wpzmfhtm@naver.com>
2024-11-29 12:13:21 +0900

    jdbc(회원가입 및 로그인)

--- java_04/src/jdbc/Ex01/_02_MemberService.java
+++ java_04/src/jdbc/Ex01/_02_MemberService.java
@@ -6,7 +6,7 @@ public class _02_MemberService {
 	public static void main(String[] args) {
 		
 		// 메서드호출 => 참조변수.메서드명();
-		_02_MemberDAOImpl dao = new _02_MemberDAOImpl();
+		_02_MemberDAO dao = new _02_MemberDAOImpl();
 		_02_MemberDTO dto = new _02_MemberDTO();
 		Scanner sc = new Scanner(System.in);
 		
