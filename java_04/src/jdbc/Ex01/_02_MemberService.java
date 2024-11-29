4e03b606a0148f3c028006700d3d71e165f37066
wpzmfhtm <wpzmfhtm@naver.com>
2024-11-29 13:00:26 +0900

    jdbc(로그인 및 회원가입)

--- java_04/src/jdbc/Ex01/_02_MemberService.java
+++ java_04/src/jdbc/Ex01/_02_MemberService.java
@@ -9,64 +9,69 @@ public class _02_MemberService {
 		_02_MemberDAO dao = new _02_MemberDAOImpl();
 		_02_MemberDTO dto = new _02_MemberDTO();
 		Scanner sc = new Scanner(System.in);
+		boolean isconnect = true; // 접속여부
 		
-		System.out.println("*----------------*");
-		System.out.println("로그인을 하시겠습니까?");
-		System.out.println("*----------------*");
-		System.out.print("◇ yes / no : ");
-		String login = sc.nextLine();
-		boolean islogin = false;
-		
-		switch(login) {
-			case "yes":
-				int cnt = dao.login(sc);
-				
-				if(cnt == 1) {
-					islogin = true;
-				}else {
-					islogin = false;
-				}
-				
-				while(islogin) {
-				System.out.println("*---------------------------------------------*");
-				System.out.println("1. 추가  2. 수정  3. 삭제  4. 조회   5. 종료");
-				System.out.println("*---------------------------------------------*");
-				System.out.print("◇ 메뉴 선택 : ");
-				int menu = sc.nextInt();
-				sc.nextLine();
-				switch(menu) {
-					case 1:
-						dao.memberInsert(dto, sc);
-						break;
-					case 2:
-						dao.memberUpdate(sc);
-						break;
-					case 3: 
-						dao.memberDelete(sc);
-						break;
-					case 4:
-						System.out.println("전체 회원 정보입니다.");
-						dao.memberSelect(sc);
-						break;
-					case 5:
-						System.out.println("시스템을 종료합니다.");
-						return; // 반복문 종료
-					default:
-						System.out.println("잘못 입력하셨습니다. 다시 해주십시오");
-						break;
+		while(isconnect) {
+			System.out.println("*----------------*");
+			System.out.println("로그인을 하시겠습니까?");
+			System.out.println("*----------------*");
+			System.out.print("◇ yes / no : ");
+			String login = sc.nextLine();
+			boolean islogin = false; // 로그인 여부
+			
+			switch(login) {
+				case "yes":
+					int cnt = dao.login(sc);
+					
+					if(cnt == 1) {
+						islogin = true;
+					}else {
+						islogin = false;
 					}
+					
+					while(islogin) {
+					System.out.println("*---------------------------------------------*");
+					System.out.println("1. 추가  2. 수정  3. 삭제  4. 조회   5. 종료");
+					System.out.println("*---------------------------------------------*");
+					System.out.print("◇ 메뉴 선택 : ");
+					int menu = sc.nextInt();
+					sc.nextLine();
+					switch(menu) {
+						case 1:
+							dao.memberInsert(dto, sc);
+							break;
+						case 2:
+							dao.memberUpdate(sc);
+							break;
+						case 3: 
+							dao.memberDelete(sc);
+							break;
+						case 4:
+							System.out.println("전체 회원 정보입니다.");
+							dao.memberSelect(sc);
+							break;
+						case 5:
+							System.out.println("시스템을 종료합니다.");
+							isconnect = false;
+							break;
+						default:
+							System.out.println("잘못 입력하셨습니다. 다시 해주십시오");
+							isconnect = false;
+							break;
+						}
+					}
+					break;
+					
+				case "no":
+					System.out.println("안녕히 가십시오~~~");
+					isconnect = false;
+					break;
+					
+				default :
+					System.out.println("yes나 no중에서 입력을 해주십시오");
+					isconnect = true;
+					break;
 				}
-				break;
-				
-			case "no":
-				System.out.println("안녕히 가십시오~~~");
-				System.exit(0);
-				break;
-				
-			default :
-				System.out.println("yes나 no중에서 입력을 해주십시오");
-				break;
+			}
 		}
-		
-	}
 }
